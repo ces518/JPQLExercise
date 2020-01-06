c9506369b2866369280ce14f47cf4d0cb2210ecb
ces518 <pupupee9@gmail.com>
2020-01-06 14:42:09 +0900

    프로젝션

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -18,6 +18,10 @@ public class JpaMain {
         tx.begin();
 
         try {
+            /**
+             * JPQL 기본문법
+             */
+            /*
             TypedQuery<Member> memberQuery = em.createQuery("select m from Member m", Member.class);
 
             // username은 String, age 는 int 이기 때문에 타입 정보를 명확하게 줄 수 없다.
@@ -36,6 +40,39 @@ public class JpaMain {
             // setParameter 를 활용하여 바인딩 해준다.
             memberTypedQuery.setParameter("username", "member1");
             Member singleResultMember = memberTypedQuery.getSingleResult();
+
+             */
+
+            /**
+             * 프로젝션
+             *
+             */
+            // 엔티티 프로젝션을 사용하면 해당 엔티티들은 모두 영속성 컨텍스트에서 관리된다.
+            List<Member> members = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+
+            // JOIN 할 경우 JPQL 에도 join을 같이 써주어야 한다.
+            // JOIN을 하지 않아도, 조인이 되어 결과같이 나가는 경우가 있지만 JPQL에도 직접 명시적으로 해주는것이 좋음
+            Team findTeam = em.createQuery("select t from Member m join m.team t", Team.class)
+                    .getSingleResult();
+
+            // 임베디드 타입 프로젝션
+            Address findAddress = em.createQuery("select o.address from Order o", Address.class)
+                    .getSingleResult();
+
+            // 스칼라 타입 프로젝션
+            List resultList = em.createQuery("select distinct m.username, m.age from Member m")
+                    .getResultList();
+
+            // 1.Query 타입을 사용하면 내부적인 값들은 Object 배열로 리턴된다.
+            // 2.제네릭을 사용하여 이 과정을 생략할 수 있다.
+            Object o = resultList.get(0);
+            Object[] result = (Object[]) o;
+
+            // 3.new Operation 사용
+            // DTO를 활용하여 DTO 타입으로 받아온다.
+            List<MemberDTO> memberDtos = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
---
+++ src/main/java/jpql/MemberDTO.java
@@ -0,0 +1,33 @@
+package jpql;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: june
+ * Date: 06/01/2020
+ * Time: 2:38 오후
+ **/
+public class MemberDTO {
+    private String username;
+    private int age;
+
+    public MemberDTO(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
